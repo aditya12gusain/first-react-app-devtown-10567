7946ede26f7eca9dc6b8f2f45deb224c9a0b9eea
aditya12gusain <adisandi3112002@gmail.com>
2022-09-07 21:04:33 +0530

    completed basics

--- src/pages/ClassComponent.jsx
+++ src/pages/ClassComponent.jsx
@@ -2,10 +2,46 @@ import React from "react";
 import BaseHoc from "../hoc/BaseHoc";
 
 class ClassComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Aditya",
+      age: 20,
+    };
+
+    console.log("Constructor: ");
+  }
+
+  componentDidMount() {
+    console.log("Component Did Mount: ");
+  }
+
+  componentDidUpdate() {
+    console.log("Component Did Update?");
+  }
+
+  componentWillUnmount() {
+    console.log("Component Did Unmount");
+  }
+
   render() {
+    console.log("Render: ");
+
     return (
       <div>
         <p>This is Class Component</p>
+        <button
+          onClick={() =>
+            this.setState({
+              ...this.state,
+              age: 22,
+            })
+          }
+        >
+          Add 1 to age
+        </button>
+        <h1>{this.state.age}</h1>
       </div>
     );
   }
--- src/pages/FunctionalComponent.jsx
+++ src/pages/FunctionalComponent.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import BaseHoc from "../hoc/BaseHoc";
 
 const FunctionalComponent = (props) => {
   const [count, setCount] = useState(0);
   const [changeName, setChangeName] = useState("");
+
+  // const prevRef = useRef();
 
   const subtractConst = () => {
     setCount(count - 1);
@@ -11,11 +13,36 @@ const FunctionalComponent = (props) => {
 
   const { name, age, company, setName } = props;
 
+  useEffect(() => {
+    console.log("Component did mount");
+  }, []);
+
+  useEffect(() => {
+    console.log("Component Did update");
+  });
+
+  useEffect(() => {
+    console.log("Only on changing name");
+  }, [changeName]);
+
+  useEffect(() => {
+    console.log("Only on props change");
+  }, [props]);
+
+  // useEffect(() => {
+  //   console.log(prevRef.current.innerHTML === count);
+  // }, [count]);
+
+  useEffect(() => {
+    return console.log("Component did unmount");
+  }, []);
+
   return (
     <div>
       <p>This is Functional Component</p>
       <button onClick={() => setCount(count + 1)}>Click me to Add 1</button>
       <button onClick={subtractConst}>Click me to Subtract 1</button>
+      {/* <h1 ref={prevRef}>{count}</h1> */}
       <h1>{count}</h1>
       <h1>
         My name is {name} I am {age}, I work for {company}
